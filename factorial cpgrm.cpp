be56af7eb715a5bab76a8f9019e546e4ca22e823
shunu123 <122294200+shunu123@users.noreply.github.com>
2023-10-12 08:41:07 +0530

    Update factorial cpgrm.cpp

--- factorial cpgrm.cpp
+++ factorial cpgrm.cpp
@@ -1,13 +1,1 @@
-#include<stdio.h>
-int main ()
-{
-	int i,fact=1,number;
-	printf("enter a number:");
-	scanf("%d",&number);
-	for(i=1;i<=number;i++)
-	{
-		fact=fact*i;
-	}
-	printf("factorial of %d is:%d",number,fact);
-	return 0;
-}
+
